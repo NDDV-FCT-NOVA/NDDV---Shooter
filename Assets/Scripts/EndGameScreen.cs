91624c27dfb3712daf72d0c31b8f11cc8d7f41be
pgpais <pgpaisdev@gmail.com>
2020-10-23 16:13:09 +0100

    Some changes so making new levels is easier

--- Assets/Scripts/EndGameScreen.cs
+++ Assets/Scripts/EndGameScreen.cs
@@ -2,18 +2,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class EndGameScreen : MonoBehaviour
 {
     [SerializeField] private GameObject endGameObjects;
-    
+
+    private GameObject _instantiatedEndGameObjects;
     
     // Start is called before the first frame update
-    void Start()
+
+    private void Awake()
+    {
+        SceneManager.sceneLoaded += NewLevel;
+    }
+
+    private void Start()
     {
         
     }
 
+    public void NewLevel(Scene arg0, LoadSceneMode loadSceneMode)
+    {
+        Transform canvas = GameObject.Find("Canvas").GetComponent<Transform>();
+        _instantiatedEndGameObjects = Instantiate(endGameObjects, canvas);
+        _instantiatedEndGameObjects.SetActive(false);
+    }
+    
     private void OnEnable()
     {
         GameController.GameEnded += ShowEndScreen;
--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,14 +1,32 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using Random = UnityEngine.Random;
 
 public class GameController : MonoBehaviour
 {
     public delegate void GameEnd();
     public static event GameEnd GameEnded;
 
+    private void Start()
+    {
+        
+    }
+
     public static void EndGame()
     {
         GameEnded?.Invoke();
+    }
+
+    public static void NextScene()
+    {
+        int nextSceneIdx = Random.Range(0, SceneManager.sceneCountInBuildSettings);
+        while (nextSceneIdx == SceneManager.GetActiveScene().buildIndex)
+        {
+            nextSceneIdx = Random.Range(0, SceneManager.sceneCountInBuildSettings);
+        }
+        SceneManager.LoadScene(nextSceneIdx);
     }
 }
